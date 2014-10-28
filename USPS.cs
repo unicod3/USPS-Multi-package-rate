4ce7a9703f8bef980607d15d8051eec48c331842
Sinan Ülker <unicod3@hotmail.com>
2014-10-28 06:09:29 +0200

    Update USPS.cs

--- USPS.cs
+++ USPS.cs
@@ -6,10 +6,10 @@
         private WebClient wsClient = new WebClient();
         //User ID obtained from USPS.
         public string USPS_UserID = ConfigurationManager.AppSettings["USPS_USERID"];
-		//Maximum package per request
-		private const int batchLimit = 25;
+	//Maximum package per request
+	private const int batchLimit = 25;
 
-		    /**
+	/**
          * Sends Request and gets reply from USPS
          **/
         private string GetResponseFromUSPS(string USPS_Request)
@@ -22,57 +22,57 @@
                 strResponse += (char)oItem;
             return strResponse;
         } 
+	
+	/**
+	 * Generates request string for getting Rate From USPS 
+	 */ 
+	public string  GenerateRatev4Request(List<myClass> packages)
+	{ 
+		string response = "";	
+		string request = baseURL + string.Format("?API=RateV4&XML=<RateV4Request USERID=\"{0}\">", USPS_UserID);
+		request += "<Revision />"; //I don't use any special services so I don't use Revision element
 		
-		/**
-		 * Generates request string for getting Rate From USPS 
-		 */ 
-		public string  GenerateRatev4Request(List<myClass> packages)
-		{ 
-			string response = "";	
-			string request = baseURL + string.Format("?API=RateV4&XML=<RateV4Request USERID=\"{0}\">", USPS_UserID);
-					request += "<Revision />"; //I don't use any special services so I don't use Revision element
-					
-					int counter = 1;  //Define a counter 
-					foreach (var package in packages)
-					{  	//loop trough each package element that we have and add them into request string
-						
-						request += string.Format("<Package ID=\"{0}\">", package.ID); //must be unique
-						request += string.Format("<Service>{0}</Service>", package.Service);
-						request += string.Format("<ZipOrigination>{0}</ZipOrigination>", package.FromOrigin);
-						request += string.Format("<ZipDestination>{0}</ZipDestination>",  package.FromDest);
-						request += string.Format("<Pounds>{0}</Pounds>", package.PackageWeightLB);
-						request += string.Format("<Ounces>{0}</Ounces>", package.PackageWeightOnz);
-						request += string.Format("<Container>{0}</Container>",  package.Container);
-						request += string.Format("<Size>{0}</Size>",  package.Size);
-						request += string.Format("<Width>{0}</Width>", package.Width);
-						request += string.Format("<Length>{0}</Length>", package.Length);
-						request += string.Format("<Height>{0}</Height>", package.Height);
-						request += string.Format("<Girth>{0}</Girth>", package.Girth);
-						request += string.Format("</Package>"); 
-						
-						if (counter % batchLimit == 0)
-						{//if number of packages exceed the USPS limit  
-							request += string.Format("</RateV4Request>"); //put an end to xml request 
-						    
-							 //Send the request to USPS and get response
-							 response = GetResponseFromUSPS(request);  
-							 HandleResponseAsYouWish(response); //Handle the response as you want with another method
-							 //Create a new request for the rest of packages
-							request = baseURL + string.Format("?API=RateV4&XML=<RateV4Request USERID=\"{0}\" PASSWORD=\"{1}\" >", USPS_UserID, USPS_Password);
-							request += "<Revision />"; 
-						} 
-						counter++;
-					}
-					//put an end to xml request
-					request += string.Format("</RateV4Request>");
-				 response = GetResponseFromUSPS(request);  //Send the request to USPS and get response
+		int counter = 1;  //Define a counter 
+		foreach (var package in packages)
+		{  	//loop trough each package element that we have and add them into request string
+			
+			request += string.Format("<Package ID=\"{0}\">", package.ID); //must be unique
+			request += string.Format("<Service>{0}</Service>", package.Service);
+			request += string.Format("<ZipOrigination>{0}</ZipOrigination>", package.FromOrigin);
+			request += string.Format("<ZipDestination>{0}</ZipDestination>",  package.FromDest);
+			request += string.Format("<Pounds>{0}</Pounds>", package.PackageWeightLB);
+			request += string.Format("<Ounces>{0}</Ounces>", package.PackageWeightOnz);
+			request += string.Format("<Container>{0}</Container>",  package.Container);
+			request += string.Format("<Size>{0}</Size>",  package.Size);
+			request += string.Format("<Width>{0}</Width>", package.Width);
+			request += string.Format("<Length>{0}</Length>", package.Length);
+			request += string.Format("<Height>{0}</Height>", package.Height);
+			request += string.Format("<Girth>{0}</Girth>", package.Girth);
+			request += string.Format("</Package>"); 
+			
+			if (counter % batchLimit == 0)
+			{//if number of packages exceed the USPS limit  
+				request += string.Format("</RateV4Request>"); //put an end to xml request 
+			    
+				 //Send the request to USPS and get response
+				 response = GetResponseFromUSPS(request);  
 				 HandleResponseAsYouWish(response); //Handle the response as you want with another method
+				 //Create a new request for the rest of packages
+				request = baseURL + string.Format("?API=RateV4&XML=<RateV4Request USERID=\"{0}\" PASSWORD=\"{1}\" >", USPS_UserID, USPS_Password);
+				request += "<Revision />"; 
+			} 
+			counter++;
 		}
-		
-		private void HandleResponseAsYouWish(string response)
+		//put an end to xml request
+		request += string.Format("</RateV4Request>");
+	 response = GetResponseFromUSPS(request);  //Send the request to USPS and get response
+	 HandleResponseAsYouWish(response); //Handle the response as you want with another method
+	}
+	
+	private void HandleResponseAsYouWish(string response)
         {
 			    //Do what you want to do with the response
             throw new NotImplementedException();
         }
 
-	}
+}
